6130c1b02a551b815361c23ae291d71794c7de25
Alejandro Guerra <guerramanuel299@gmail.com>
2025-02-04 21:25:21 -0500

    Creating card action component at 9:25

---
+++ src/components/CardActions.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const CardActions = () => {
+  return (
+    <View style={Styles.Container}>
+      <Text>Hola</Text>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  Container: {
+    borderWidth: 1,
+    margin: 4,
+    padding: 10,
+    borderRadius: 7,
+    elevation: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+});
--- src/screens/PrincipalScreen.tsx
+++ src/screens/PrincipalScreen.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {CardActions} from '../components/CardActions';
 
 export const PrincipalScreen = () => {
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
-      <Text>PrincipalScreen</Text>
+
+      <CardActions />
     </View>
   );
 };
